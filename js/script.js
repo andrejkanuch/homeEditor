cc1f31379cc2246f62ab4688769e67e86c0f1213
Andrej Kanuch <Andrej Kanuch@resco2.local>
2019-09-19 13:30:06 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 (function() {
-  const nodelist = document.getElementById('list-section').querySelectorAll('.nested');
-  const divyArray = Array.from(nodelist)
-  dragula([document.querySelector('#list-section .container'), document.getElementById('available-items')].concat(divyArray),{
-    copy: function (el, source) {
-      console.log(el,source);
-      return source === document.getElementById('available-items')
-    },
-    accepts: function (el, target) {
-      return target !== document.getElementById('available-items')
-    },
-    removeOnSpill: function(el,target){
-      console.log(el,target);
-    }
-  });
-  })();
+    const nodelist = document.getElementById('list-section').querySelectorAll('.nested');
+    const divyArray = Array.from(nodelist)
+    dragula([document.querySelector('#list-section .container'), document.getElementById('available-items')].concat(divyArray), {
+        copy: function(el, source) {
+            console.log(source)
+            return source === document.getElementById('available-items')
+        },
+        accepts: function(el, target) {
+            return target !== document.getElementById('available-items')
+        },
+        removeOnSpill: function(el, target) {
+            console.log(el, target);
+        }
+    }).on('drop', function(el, source) {
+        console.log(el.classList.contains('ex-moved'), el);
+        if (!el.classList.contains('ex-moved') && el.children[0]) {
+            el.children[0].insertAdjacentHTML('afterbegin', '<span class="drag-image"></div>');
+            el.className += ' ex-moved';
+        }
+    });
+})();
