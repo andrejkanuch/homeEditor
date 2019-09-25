8d60984f1537d57dbf068b0df969b6aecec3a85a
Andrej Kanuch <Andrej Kanuch@resco2.local>
2019-09-25 11:15:50 +0200

    add clicking element

--- js/script.js
+++ js/script.js
@@ -13,10 +13,36 @@
             console.log(el, target);
         }
     }).on('drop', function(el, source) {
-        console.log(el.classList.contains('ex-moved'), el);
         if (!el.classList.contains('ex-moved') && el.children[0]) {
+            el.children[0].insertAdjacentHTML('afterbegin', ' <a class="home-button"><span class="home-image unactive"></span></a>');
             el.children[0].insertAdjacentHTML('afterbegin', '<span class="drag-image"></div>');
+            el.children[0].insertAdjacentHTML('beforeend', '<span class="delete-image"></div>');
             el.className += ' ex-moved';
         }
     });
 })();
+$(document).ready(function() {
+    $(document).on("click", ".home-button", function() {
+        $('.home-button').children().removeClass('active').addClass('unactive');
+        $('.home-button').removeClass('show-home').addClass('hide-home');
+
+        $(this).removeClass('hide-home').addClass('show-home');
+        $(this).children().removeClass('unactive').addClass('active');
+    });
+});
+
+// $('.home-button').click(function() {
+//     $('.home-button').children().removeClass('active').addClass('unactive');
+//     $('.home-button').removeClass('show-home').addClass('hide-home');
+
+//     $(this).removeClass('hide-home').addClass('show-home');
+//     $(this).children().removeClass('unactive').addClass('active');
+
+// });
+
+$(document).ready(function() {
+
+    $(document).on("click", ".delete-image", function() {
+        $(this).parent().remove();
+    });
+});
