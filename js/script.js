893ed39ca65a189c49c3f9fcf901bc8cf2b24dfd
Andrej Kanuch <Andrej Kanuch@resco2.local>
2019-10-03 12:54:34 +0200

    ADD home clickable button

--- js/script.js
+++ js/script.js
@@ -3,38 +3,37 @@
     const divyArray = Array.from(nodelist)
     dragula([document.querySelector('#list-section .container'), document.getElementById('special-items'), document.getElementById('available-items')].concat(divyArray), {
         copy: function(el, source) {
-            console.log(source)
             return source === document.getElementById('available-items') || document.getElementById('special-items')
         },
         accepts: function(el, target) {
-            return target !== document.getElementById('available-items') || document.getElementById('special-items')
+            return target !== document.getElementById('available-items') && target !== document.getElementById('special-items')
         },
         removeOnSpill: function(el, target) {
-            console.log(el, target);
+            // console.log(el, target);
         }
     }).on('drop', function(el, source) {
         if (!el.classList.contains('ex-moved') && el.children[0]) {
-            // el.children[0].insertAdjacentHTML('afterbegin', ' <a class="home-button"><span class="home-image unactive"></span></a>');
-            // el.children[0].insertAdjacentHTML('afterbegin', '<span class="drag-image"></div>');
-            // el.children[0].insertAdjacentHTML('beforeend', '<span class="delete-image"></div>');
-            el.children[0].insertAdjacentHTML('beforeend',
+            let groupButtons =
                 '<div class="buttons-group">' +
                 '<span class="edit-image"></span>' +
                 '<span class="home-image"></span>' +
                 '<span class="delete-image"></span>' +
-                '</div>');
+                '</div>'
+            el.children[0].insertAdjacentHTML('beforeend', groupButtons);
 
             el.className += ' ex-moved';
         }
     });
 })();
 $(document).ready(function() {
-    $(document).on("click", ".home-button", function() {
-        $('.home-button').children().removeClass('active').addClass('unactive');
-        $('.home-button').removeClass('show-home').addClass('hide-home');
+    $(document).on("click", ".home-image", function() {
+        $('.home-image').css("opacity", "0.1");
+        $(this).css("opacity", "1");
+        // $('.home-button').children().removeClass('active').addClass('unactive');
+        // $('.home-button').removeClass('show-home').addClass('hide-home');
 
-        $(this).removeClass('hide-home').addClass('show-home');
-        $(this).children().removeClass('unactive').addClass('active');
+        // $(this).removeClass('hide-home').addClass('show-home');
+        // $(this).children().removeClass('unactive').addClass('active');
     });
 
     $(document).on("click", ".delete-image", function() {
