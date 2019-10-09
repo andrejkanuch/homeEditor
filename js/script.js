0cebdde94fd188ea6f0c021c6c26cb21bf7e4598
Andrej Kanuch <Andrej Kanuch@resco2.local>
2019-10-09 10:13:25 +0200

    add icon after drop

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 (function() {
     const nodelist = document.getElementById('list-section').querySelectorAll('.nested');
     const divyArray = Array.from(nodelist)
-        // dragula([document.querySelector('#list-section .container')], {
-        //     removeOnSpill: true
-        // });
     dragula([document.querySelector('#list-section .container'), document.getElementById('special-items'), document.getElementById('available-items')].concat(divyArray), {
         copy: function(el, source) {
             return source === document.getElementById('available-items') || source === document.getElementById('special-items')
@@ -13,13 +10,17 @@
         },
     }).on('drop', function(el, source) {
         if (!el.classList.contains('ex-moved') && el.children[0]) {
+            let dragImage = el.children[0].getElementsByClassName('drag-image');
+            console.log(dragImage);
             let groupButtons =
                 '<div class="buttons-group">' +
                 '<span class="edit-image"></span>' +
                 '<span class="home-image"></span>' +
                 '<span class="delete-image"></span>' +
                 '</div>'
+            let defaultIcon = '<span class="settings-image"></span>';
             el.children[0].insertAdjacentHTML('beforeend', groupButtons);
+            if (dragImage[0]) { dragImage[0].insertAdjacentHTML('afterend', defaultIcon); }
 
             el.className += ' ex-moved';
         }
@@ -38,8 +39,8 @@ $(document).ready(function() {
         let item = $(this).closest('.ex-moved');
         let nestedItem = item.parent()
         item.remove();
-
-        if (nestedItem.children().length == 0) {
+        console.log(nestedItem.parent().hasClass('sub-section'))
+        if (nestedItem.children().length == 0 && nestedItem.parent().hasClass('sub-section')) {
             nestedItem.addClass("empty-folder")
         }
     });
